c93f28f1a6259fa6a6178e90e055bf3aee3a6775
Tristan <Tristan@MacBook-Pro.local>
2019-09-25 14:09:43 +1000

    Basic angular routing & app depedencies: bootstrap & socket.io

--- Chatter/src/app/app-routing.module.ts
+++ Chatter/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// for 'const routes: Route = []
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ControlsComponent } from './controls/controls.component';
+import { ChatComponent } from './chat/chat.component';
+//
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'controls', component: ControlsComponent},
+  {path: 'chat', component: ChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
