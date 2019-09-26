968cf8c60d8d2a9c71da1479dc92c79a89525203
Tristan <Tristan@MacBook-Pro.local>
2019-09-26 10:59:11 +1000

    Front-end of profile page in progress.

--- Chatter/src/app/profile/profile.component.ts
+++ Chatter/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed = false;
+
+  groups = [
+    {
+      name:"group1", 
+      channels:[
+        {name:"channel1"}, 
+        {name:"channel2"}, 
+        {name:"channel3"}
+      ]
+    },
+    {
+      name:"group2", 
+      channels:[
+      ]
+    },
+    {
+      name:"group3", 
+      channels:[
+        {name:"channelA"}, 
+        {name:"channelB"}, 
+        {name:"channelC"}
+      ]
+    }
+  ]
+
+  constructor(private tag: ElementRef) {}
 
   ngOnInit() {
+  }
+
+  toggleCollapse(groupName){
+    console.log("+-------------------------------------------\\");
+    console.log("| A group div was clicked.");
+    console.log("|     value inputed for groupName->", groupName);
+    console.log("+-------------------------------------------/");
+    console.log();
+    let temp = this.tag.nativeElement.querySelector("#" + groupName);
+    let i;
+    let result = false;
+    for(i=0; i < temp.classList.length; i++){
+      let x = temp.classList[i]
+      if(x == "collapse"){
+        result = true;
+        break
+      }
+    }
+    if(result){
+      temp.classList.remove("collapse");
+    } else {
+      temp.classList.add("collapse");
+    }
   }
 
 }
